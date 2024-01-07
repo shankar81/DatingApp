0e0e254ebd7cab923c4324cae73395b8ef5c7e50
shankar sawant <shankarsawant81@gmail.com>
2024-01-07 18:49:46 +0530

    added bootstrap and https

--- API/Program.cs
+++ API/Program.cs
@@ -27,6 +27,8 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
