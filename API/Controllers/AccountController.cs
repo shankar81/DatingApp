9a06f3077e95b56c09f55eb2b1871a76603b6809
Shankar Sawant <shankarsawant@Shankars-MacBook-Pro.local>
2024-01-10 12:17:06 +0530

    added login and register

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController : BaseController
+{
+    private readonly DataContext context;
+
+    public AccountController(DataContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(RegisterDto registerDto)
+    {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username already registered.");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new User
+        {
+            UserName = registerDto.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<User>> Login(LoginDto loginDto)
+    {
+
+        var user = await context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
+
+        if (user == null) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++) {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+        }
+
+        return user;
+    }
+
+    public async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(user => user.UserName == username.ToLower());
+    }
+}
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // api/users
-public class UsersController : ControllerBase
+public class UsersController : BaseController
 {
     private readonly DataContext _context;
 
