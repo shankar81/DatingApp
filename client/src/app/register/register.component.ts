f94687ed8857cfafdf494525bec35f72c9d9da9e
shankar sawant <shankarsawant81@gmail.com>
2024-02-27 10:20:20 +0530

    WIP forms

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -14,17 +21,48 @@ export class RegisterComponent implements OnInit {
   @Output() cancelEvent = new EventEmitter();
 
   model: any = {};
+  registerFormGroup: FormGroup = new FormGroup({});
 
   constructor(private accoutnService: AccountService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerFormGroup = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8),
+      ]),
+      confirmPassword: new FormControl('', [
+        Validators.required,
+        this.matchValues('password'),
+      ]),
+    });
+
+    this.registerFormGroup.controls['password'].valueChanges.subscribe({
+      next: () => this.registerFormGroup.controls['confirmPassword'].updateValueAndValidity(),
+    });
+  }
+
+  matchValues(matchTo: string) {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null
+        : { notMatching: true };
+    };
+  }
 
   onRegister() {
-    this.accoutnService.onRegister(this.model).subscribe({
-      next: () => {
-        this.onCancel();
-      },
-    });
+    console.log(this.registerFormGroup.status, this.registerFormGroup.value);
+    // this.accoutnService.onRegister(this.model).subscribe({
+    //   next: () => {
+    //     this.onCancel();
+    //   },
+    // });
   }
 
   onCancel() {
